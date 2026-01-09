f74535152437f1415f0a5d359f32497e1c11e6f4
ArsonManStudios <arsonmanstudios@gmail.com>
2026-01-09 09:37:13 +0500

    Added more colours and set min amplitude
    The colours red and blue have been added with the functionality to choose which colours to be shown on the wave.
    The minimum amplitude of the wave has also been set to 0.

--- main.py
+++ main.py
@@ -7,10 +7,32 @@ import pygame
 pygame.init()
 clock = pygame.time.Clock()
 run = True
+RED   = [(255, 0, 0),
+         (156, 0, 0),
+         (71, 0, 0)]
 GREEN = [(0, 255, 0),
          (0, 156, 0),
          (0, 71, 0)]
+BLUE  = [(0, 0, 255),
+         (0, 0, 156),
+         (0, 0, 71)]
 BLACK = (0, 0, 0)
+
+# 0: Red
+# 1: Green
+# 2: Blue
+rgb_choice = [1, 1, 1]
+seperation = 0.25
+
+points_red1 = [(-10, -10), (-10, -10)]
+points_red2 = [(-10, -10), (-10, -10)]
+points_red3 = [(-10, -10), (-10, -10)]
+points_green1 = [(-10, -10), (-10, -10)]
+points_green2 = [(-10, -10), (-10, -10)]
+points_green3 = [(-10, -10), (-10, -10)]
+points_blue1 = [(-10, -10), (-10, -10)]
+points_blue2 = [(-10, -10), (-10, -10)]
+points_blue3 = [(-10, -10), (-10, -10)]
 
 size = (1000, 700)
 screen = pygame.display.set_mode(size)
@@ -18,7 +40,7 @@ pygame.display.set_caption('Audio Wave Visualization')
 h = screen.get_height()
 w = screen.get_width()
 
-file_path = 'points_of_authority.mp3'
+file_path = 'cure_for_teh_itch.mp3'
 chunk_size = 2048
 point_freq = w
 
@@ -65,13 +87,28 @@ with sd.OutputStream(
             amp = info(chunk)
             dtime = pygame.time.get_ticks()
             points = np.linspace(0, w, point_freq)
-            points1 = np.column_stack((points, func1(points, map_value(amp, 0, 0.3, 25, 100),  dtime/150)))
-            points2 = np.column_stack((points, func2(points, map_value(amp, 0, 0.3, 25, 150),  dtime/250)))
-            points3 = np.column_stack((points, func3(points, map_value(amp, 0, 0.3, 25, 200),  dtime/500)))
+            if rgb_choice[0]:
+                points_red1 = np.column_stack((points, func1(points, map_value(amp, 0, 0.3, 0, 100),  dtime/150+seperation)))
+                points_red2 = np.column_stack((points, func2(points, map_value(amp, 0, 0.3, 0, 150),  dtime/250+seperation)))
+                points_red3 = np.column_stack((points, func3(points, map_value(amp, 0, 0.3, 0, 200),  dtime/500+seperation)))
+            if rgb_choice[1]:
+                points_green1 = np.column_stack((points, func1(points, map_value(amp, 0, 0.3, 0, 100),  dtime/150)))
+                points_green2 = np.column_stack((points, func2(points, map_value(amp, 0, 0.3, 0, 150),  dtime/250)))
+                points_green3 = np.column_stack((points, func3(points, map_value(amp, 0, 0.3, 0, 200),  dtime/500)))
+            if rgb_choice[2]:
+                points_blue1 = np.column_stack((points, func1(points, map_value(amp, 0, 0.3, 0, 100),  dtime/150-seperation)))
+                points_blue2 = np.column_stack((points, func2(points, map_value(amp, 0, 0.3, 0, 150),  dtime/250-seperation)))
+                points_blue3 = np.column_stack((points, func3(points, map_value(amp, 0, 0.3, 0, 200),  dtime/500-seperation)))
             screen.fill(BLACK)
-            pygame.draw.lines(screen, GREEN[2], False, points3, 2)
-            pygame.draw.lines(screen, GREEN[1], False, points2, 2)
-            pygame.draw.lines(screen, GREEN[0], False, points1, 2)
+            pygame.draw.lines(screen, RED[2], False, points_red3, 2)
+            pygame.draw.lines(screen, GREEN[2], False, points_green3, 2)
+            pygame.draw.lines(screen, BLUE[2], False, points_blue3, 2)
+            pygame.draw.lines(screen, RED[1], False, points_red2, 2)
+            pygame.draw.lines(screen, GREEN[1], False, points_green2, 2)
+            pygame.draw.lines(screen, BLUE[1], False, points_blue2, 2)
+            pygame.draw.lines(screen, RED[0], False, points_red1, 2)
+            pygame.draw.lines(screen, GREEN[0], False, points_green1, 2)
+            pygame.draw.lines(screen, BLUE[0], False, points_blue1, 2)
             i += chunk_size
         pygame.display.flip()
         clock.tick(60)
